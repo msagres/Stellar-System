5d20c18f8e4067927c96b889424bacda38616626
Miguel Costa <ambcslb1@gmail.com>
2019-06-05 19:56:14 +0200

    loader added; cleaned; commentied

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+// LOADER
+$(window).on("load", function () {
+    $(".loadercontainer").fadeOut("slow");
+});
+// LOADER
+
+
+// PARTICLES
 $(document).ready(function () {
     $(".particules").delay(50).animate({
         "opacity": "1"
     }, 1600);
 });
+// PARTICLES
 
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-      document.getElementById("stellarSystem").className = 'slideDown';
+
+// TITTLE
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
+        document.getElementById("stellarSystem").className = 'slideDown';
     }, 200);
-  }, false);
+}, false);
+// TITTLE
